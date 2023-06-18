ecba12f97e9f3a7d6b93262787a2cdb07aa5b825
leukipp <leukipp@outlook.com>
2023-06-18 19:44:18 +0200

    fix systray icon layout

--- ui/icon.go
+++ ui/icon.go
@@ -17,7 +17,7 @@ import (
 
 var (
 	iconSize     int = 256 // Size of systray icon
-	iconMargin   int = 12  // Margin of systray icon
+	iconMargin   int = 36  // Margin of systray icon
 	layoutMargin int = 12  // Margin of layout rectangles
 )
 
@@ -34,36 +34,36 @@ func UpdateIcon(ws *desktop.Workspace) {
 
 	// Initialize image
 	color := image.Uniform{rgba("icon_foreground")}
-	icon := image.NewRGBA(image.Rect(0, 0, iconSize+2*iconMargin, iconSize+2*iconMargin))
+	icon := image.NewRGBA(image.Rect(0, 0, iconSize, iconSize))
 
 	// Draw background rectangle
 	x0, y0, x1, y1 := iconMargin, iconMargin, iconSize-iconMargin, iconSize-iconMargin
-	draw.Draw(icon, image.Rect(x0, y0, x1, y1), &image.Uniform{rgba("icon_background")}, image.Point{}, draw.Src)
+	draw.Draw(icon, icon.Bounds(), &image.Uniform{rgba("icon_background")}, image.Point{}, draw.Src)
 
 	// Draw layout rectangles
 	switch name {
 	case "fullscreen":
-		draw.Draw(icon, image.Rect(x0+layoutMargin, y0+layoutMargin, x1-layoutMargin, y1-layoutMargin), &color, image.Point{}, draw.Src)
+		draw.Draw(icon, image.Rect(x0, y0, x1, y1), &color, image.Point{}, draw.Src)
 	case "vertical-left":
-		draw.Draw(icon, image.Rect(x0+layoutMargin, y0+layoutMargin, x1/2-layoutMargin/2, y1-layoutMargin), &color, image.Point{}, draw.Src)
-		draw.Draw(icon, image.Rect(x1/2+layoutMargin, y0+layoutMargin, x1-layoutMargin, y1/2-layoutMargin/2), &color, image.Point{}, draw.Src)
-		draw.Draw(icon, image.Rect(x1/2+layoutMargin, y1/2+layoutMargin, x1-layoutMargin, y1-layoutMargin), &color, image.Point{}, draw.Src)
+		draw.Draw(icon, image.Rect(x0, y0, x0+(x1-x0)/2-layoutMargin, y1), &color, image.Point{}, draw.Src)
+		draw.Draw(icon, image.Rect(x0+(x1-x0)/2+layoutMargin, y0, x1, y0+(y1-y0)/2-layoutMargin), &color, image.Point{}, draw.Src)
+		draw.Draw(icon, image.Rect(x0+(x1-x0)/2+layoutMargin, y0+(y1-y0)/2+layoutMargin, x1, y1), &color, image.Point{}, draw.Src)
 	case "vertical-right":
-		draw.Draw(icon, image.Rect(x0+layoutMargin, y0+layoutMargin, x1/2-layoutMargin/2, y1/2-layoutMargin/2), &color, image.Point{}, draw.Src)
-		draw.Draw(icon, image.Rect(x0+layoutMargin, y1/2+layoutMargin, x1/2-layoutMargin/2, y1-layoutMargin), &color, image.Point{}, draw.Src)
-		draw.Draw(icon, image.Rect(x1/2+layoutMargin, y0+layoutMargin, x1-layoutMargin, y1-layoutMargin), &color, image.Point{}, draw.Src)
+		draw.Draw(icon, image.Rect(x0, y0, x0+(x1-x0)/2-layoutMargin, y0+(y1-y0)/2-layoutMargin), &color, image.Point{}, draw.Src)
+		draw.Draw(icon, image.Rect(x0, y0+(y1-y0)/2+layoutMargin, x0+(x1-x0)/2-layoutMargin, y1), &color, image.Point{}, draw.Src)
+		draw.Draw(icon, image.Rect(x0+(x1-x0)/2+layoutMargin, y0, x1, y1), &color, image.Point{}, draw.Src)
 	case "horizontal-top":
-		draw.Draw(icon, image.Rect(x0+layoutMargin, y0+layoutMargin, x1-layoutMargin, y1/2-layoutMargin/2), &color, image.Point{}, draw.Src)
-		draw.Draw(icon, image.Rect(x0+layoutMargin, y1/2+layoutMargin, x1/2-layoutMargin/2, y1-layoutMargin), &color, image.Point{}, draw.Src)
-		draw.Draw(icon, image.Rect(x1/2+layoutMargin, y1/2+layoutMargin, x1-layoutMargin, y1-layoutMargin), &color, image.Point{}, draw.Src)
+		draw.Draw(icon, image.Rect(x0, y0, x1, y0+(y1-y0)/2-layoutMargin), &color, image.Point{}, draw.Src)
+		draw.Draw(icon, image.Rect(x0, y0+(y1-y0)/2+layoutMargin, x0+(x1-x0)/2-layoutMargin, y1), &color, image.Point{}, draw.Src)
+		draw.Draw(icon, image.Rect(x0+(x1-x0)/2+layoutMargin, y0+(y1-y0)/2+layoutMargin, x1, y1), &color, image.Point{}, draw.Src)
 	case "horizontal-bottom":
-		draw.Draw(icon, image.Rect(x0+layoutMargin, y0+layoutMargin, x1/2-layoutMargin/2, y1/2-layoutMargin/2), &color, image.Point{}, draw.Src)
-		draw.Draw(icon, image.Rect(x1/2+layoutMargin, y0+layoutMargin, x1-layoutMargin, y1/2-layoutMargin/2), &color, image.Point{}, draw.Src)
-		draw.Draw(icon, image.Rect(x0+layoutMargin, y1/2+layoutMargin, x1-layoutMargin, y1-layoutMargin), &color, image.Point{}, draw.Src)
+		draw.Draw(icon, image.Rect(x0, y0, x0+(x1-x0)/2-layoutMargin, y0+(y1-y0)/2-layoutMargin), &color, image.Point{}, draw.Src)
+		draw.Draw(icon, image.Rect(x0+(x1-x0)/2+layoutMargin, y0, x1, y0+(y1-y0)/2-layoutMargin), &color, image.Point{}, draw.Src)
+		draw.Draw(icon, image.Rect(x0, y0+(y1-y0)/2+layoutMargin, x1, y1), &color, image.Point{}, draw.Src)
 	case "disabled":
-		draw.Draw(icon, image.Rect(x0+layoutMargin, y0+layoutMargin, x0+3*layoutMargin, y1-3*layoutMargin), &color, image.Point{}, draw.Src)
-		draw.Draw(icon, image.Rect(x0+layoutMargin, y0+layoutMargin, x1-3*layoutMargin, y0+3*layoutMargin), &color, image.Point{}, draw.Src)
-		draw.Draw(icon, image.Rect(x0+3*layoutMargin+20, y0+3*layoutMargin+20, x1-layoutMargin, y1-layoutMargin), &color, image.Point{}, draw.Src)
+		draw.Draw(icon, image.Rect(x0, y0, x0+2*layoutMargin, y1-2*layoutMargin), &color, image.Point{}, draw.Src)
+		draw.Draw(icon, image.Rect(x0, y0, x1-2*layoutMargin, y0+2*layoutMargin), &color, image.Point{}, draw.Src)
+		draw.Draw(icon, image.Rect(x0+2*layoutMargin+20, y0+2*layoutMargin+20, x1, y1), &color, image.Point{}, draw.Src)
 	}
 
 	// Encode image bytes
